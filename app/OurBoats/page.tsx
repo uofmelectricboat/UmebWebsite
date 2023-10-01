212f32754cef44e043fe86f5edbc5be0f693fe56
apalo <apalo@umich.edu>
2023-10-01 18:17:01 -0400

    push

--- app/OurBoats/page.tsx
+++ app/OurBoats/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
     return (
       <div className='relative'>
-        <img src="/images/OurBoats.png?v=2" className='h-full w-full object-cover'></img>
+        <img src="/images/OurBoats.png?v=2" className='w-full h-screen object-cover'></img>
+        <div className="absolute inset-0 bg-blue-800 opacity-30"></div>
       </div>
     )
   }
