652bef12ac90c3e00506cbf3f4442ca736e45bd9
apalo <apalo@umich.edu>
2024-01-25 16:09:28 -0500

    races and sponsors updated

--- app/OurRaces/page.tsx
+++ app/OurRaces/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import TitlePhoto from "@/app/Components/TitlePhoto";
 import React from "react";
 import YouTube from "react-youtube";
+import {Button} from "flowbite-react";
 
 
 const titlePhoto = "/images/OurRaces.jpg?v=2"
@@ -19,87 +20,70 @@ export default function Home() {
     return (
         <>
             <TitlePhoto title={titleText} image={titlePhoto}/>
-            <div className="bg-blue-950 px-10">
-                <h1 className="text-yellow-500 text-4xl font-bold text-center px-20 py-20">
-                    Promoting Electric Propulsion
-                </h1>
-                <div className="text-center text-white text-2xl tracking-wider">
-                    <p className="pb-10 px-20">
-                        UM Electric Boat has competed in the ‘Promoting Electric Propulsion’ competition since 2021 and we plan on continuing this tradition—racing against top engineering schools across the country, and winning—for years to come.
-                    </p>
-                    <p className="pb-10 px-20">
-                        UMEB placed second in a heat of 10+ teams in 2021, demonstrating our strengths in both the manned and unmanned races. We hope to return in 2024 to perform even better.
-                    </p>
-                </div>
-            </div>
-            <div className="w-full">
-                <div className="relative w-full h-0" style={{ paddingBottom: '50%' }}>
-                    <Image
-                        src="/images/OurRacesRace.png"
-                        alt="Image not found"
-                        fill={true}
-                    />
-                </div>
-            </div>
-            <div className="flex">
-                <div className="w-1/2">
-                    <div className="py-20 px-40">
-                        <Image
-                            src="/images/OurRacesGraphic.png"
-                            alt="Image not found"
-                            width={700}
-                            height={700}
-                        />
+            <div className="w-full bg-stone-100">
+                <div className={"w-full flex flex-col md:flex-row justify-center items-center lg:py-6 py-2 md:py-4 "}>
+                    <div className={"w-1/4 relative order-last md:order-first"}>
+                        <img src={"/images/OurRacesGraphic.png"} alt={"Image Not Found"} className={"object-scale-down"}></img>
                     </div>
-                </div>
-                <div className="w-1/2 py-20 px-10">
-                    <p className="text-center text-2xl px-20 tracking-wide leading-relaxed">
-                        Each year, the flexible rules and talented competitors at PEP allow us to push forward and create innovative designs. In June of 2023, UMEB and our fellow competitors were tasked with completing a 5 mile race over 5 laps in a short out and back circuit.
-                    </p>
-                </div>
-            </div>
-            <div className="pt-5 pb-10">
-                <p className="text-center text-2xl px-80 tracking-wide">
-                    PEP hosts a wide variety of schools from across the country, each with a unique and innovative design each year. Some schools include:
-                </p>
-                <div className="flex items-center px-80 py-10 text-2xl">
-                    <div className="w-1/2 px-12">
-                        <ul className="list-disc ml-4">
-                            <li>Princeton University</li>
-                            <li>Texas A&M</li>
-                            <li> University of Kentucky </li>
-                        </ul>
-                    </div>
-                    <div className="w-1/2 px-12">
-                        <ul className="list-disc ml-4">
-                            <li>University of Georgia</li>
-                            <li>University of Washington</li>
-                            <li> Johns Hopkins University </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="w-screen flex items-center justify-center">
-                    <div className="relative w-1/2">
-                        <div className="relative w-full h-0" style={{ paddingBottom: '50%' }}>
-                            <Image
-                                src="/images/OurRacesMap.png"
-                                alt="Image not found"
-                                fill={true}
-                            />
+                    <div className={"w-full lg:w-1/2 pl-2 lg:pl-8 md:pl-6 sm:pl-4"}>
+                        <h1 className={"text-base lg:text-4xl text-blue-900 md:text-2xl sm:text-xl font-extrabold pb-2"}>
+                            Promoting Electric Propulsion - April 2024
+                        </h1>
+                        <p className={"text-xs lg:text-lg text-blue-950 md:text-base sm:text-xs tracking-tight leading-tight"}>
+                            UM Electric Boat has participated in the 'Promoting Electric Propulsion' competition since 2021, consistently competing against top engineering schools nationwide. In 2021, UMEB secured second place in a heat of 10+ teams, showcasing prowess in both manned and unmanned races. In June of 2023, UMEB and our fellow competitors were tasked with completing a 5-mile race over 5 laps in a short out-and-back circuit.
+                        </p>
+                        <p className={"text-xs lg:text-lg text-blue-950 md:text-base sm:text-xs tracking-tight leading-tight pt-5"}>
+                            The team aims to continue this tradition, aiming for even better results in 2024. The competition's flexible rules and talented competitors enable us to push boundaries and create innovative designs.
+                        </p>
+                        <div className={"py-2 flex md:justify-end justify-center"}>
+                            <Button gradientDuoTone={"purpleToPink"} outline href={"https://www.navalengineers.org/Education/Promoting-Electric-Propulsion-PEP"}>Check Out PEP!</Button>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="bg-blue-950 py-10">
-                <h1 className="text-4xl text-yellow-500 font-bold text-center">
-                    Monaco Energy Boat Challenge
-                </h1>
-                <p className="text-center text-2xl text-white px-60 py-10 tracking-wide leading-loose">
-                    UM Electric Boat is considering attending the Monaco Energy Boat Challenge in the near future. The event gathers more than 50 teams from universities and companies across the world. The competition seeks to further innovation in alternative propulsion fuel sources such as solar, hydrogen, wind, and more.
-                </p>
+            <div className="w-full bg-white">
+                <div className={"w-full flex flex-col md:flex-row justify-center items-center lg:py-6 py-2 md:py-4 "}>
+                    <div className={"w-full lg:w-1/2 pl-2 lg:pl-8 md:pl-6 sm:pl-4"}>
+                        <h1 className={"text-base lg:text-4xl text-blue-900 md:text-2xl sm:text-xl font-extrabold pb-2"}>
+                            Lake of the Ozarks Shootout - August 2024
+                        </h1>
+                        <p className={"text-xs lg:text-lg text-blue-950 md:text-base sm:text-xs tracking-tight leading-tight"}>
+                            The Lake of the Ozarks Shootout (LOTO) stands out as a thrilling annual spectacle for boat racing enthusiasts at the Lake of the Ozarks in Missouri, USA. Renowned as one of the largest unsanctioned boat races in the world, it's an event that showcases the sheer speed and power of high-performance boats. Racing boats regularly break speeds of over 200 mph, with the top speed recorded at 244 mph in 2014.
+                        </p>
+                        <p className={"text-xs lg:text-lg text-blue-950 md:text-base sm:text-xs tracking-tight leading-tight pt-5"}>
+                            UMEB will be attending LOTO this summer and racing as one of the few electric boats, with hopes of breaking records for the fastest electric boat to compete at LOTO.
+                        </p>
+                        <div className={"py-2 flex md:justify-start justify-center"}>
+                            <Button gradientDuoTone={"purpleToPink"} outline href={"https://www.navalengineers.org/Education/Promoting-Electric-Propulsion-PEP"}>Check Out PEP!</Button>
+                        </div>
+                    </div>
+                    <div className={"w-1/4 relative flex md:justify-end justify-center"}>
+                        <img src={"/images/OurRacesGraphic.png"} alt={"Image Not Found"} className={"object-scale-down"}></img>
+                    </div>
+                </div>
             </div>
-            {/*TODO: Fix this video*/}
-            <div className="w-full flex justify-center bg-blue-950 pb-10">
+            <div className="w-full bg-stone-100">
+                <div className={"w-full flex flex-col md:flex-row justify-center items-center lg:py-6 py-2 md:py-4 "}>
+                    <div className={"w-1/4 relative order-last md:order-first"}>
+                        <img src={"/images/OurRacesGraphic.png"} alt={"Image Not Found"} className={"object-scale-down"}></img>
+                    </div>
+                    <div className={"w-full lg:w-1/2 pl-2 lg:pl-8 md:pl-6 sm:pl-4"}>
+                        <h1 className={"text-base lg:text-4xl text-blue-900 md:text-2xl sm:text-xl font-extrabold pb-2"}>
+                            Promoting Electric Propulsion - April 2024
+                        </h1>
+                        <p className={"text-xs lg:text-lg text-blue-950 md:text-base sm:text-xs tracking-tight leading-tight"}>
+                            UM Electric Boat plans to attend the Monaco Energy Boat Challenge in the summer of 2025. The event gathers more than 50 teams from universities and companies worldwide. The competition seeks to further innovation in alternative propulsion fuel sources such as solar, hydrogen, wind, and more.
+                        </p>
+                        <p className={"text-xs lg:text-lg text-blue-950 md:text-base sm:text-xs tracking-tight leading-tight pt-5"}>
+                            UMEB plans on competing in the Sea Lab, which is a new competition specifically for novel prototypes and new designs. The team also plans on demonstrating our best speed at range in the E-Boat Rally, which is a 21 nautical mile trip between Port Hercules in Monaco and Cala del Forte in Ventimiglia in Italy.
+                        </p>
+                        <div className={"py-2 flex md:justify-end justify-center"}>
+                            <Button gradientDuoTone={"purpleToPink"} outline href={"https://energyboatchallenge.com/"}>Check Out Monaco!</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="w-full flex justify-center bg-blue-950 py-10">
                 <YouTube videoId="kEx7j_lCZ-k" opts={opts} ></YouTube>
             </div>
         </>
--- app/OurSponsors/page.tsx
+++ app/OurSponsors/page.tsx
@@ -6,107 +6,35 @@ import Image from "next/image";
 
 const sunlightSponsors = [{
     text: "The Michigan College of Engineering strives to anticipate the changes ahead and provide scientific and technological leadership - for the common good. Michigan Engineering is home to top-ranked departments that collaborate within the nation's number one public research institution - The University of Michigan.",
-    name: "MICHIGAN COLLEGE OF ENGINEERING",
-    zone: "SUNLIGHT ZONE",
+    name: "Skater",
+    zone: "Platinum",
     image: "/logos/UmichCOE.png",
     learnMore: "31 hp",
 },{
     text: "The Michigan NAME Department trains naval architects and marine engineers who lead, setting the global standard for design and research in every endeavor that touches water.",
-    name: "MICHIGAN NAVAL ARCHITECTURE & MARINE ENGINEERING",
-    zone: "SUNLIGHT ZONE",
+    name: "Liberator Boats",
+    zone: "Platinum",
     image: "/logos/MichNavalSponsor.png",
     learnMore: "31 hp",
 },{
-    text: "Cadence is a pivotal leader in electronic design, building upon more than 30 years of computational software expertise. The company applies its underlying Intelligent System Design strategy to deliver software, hardware and IP that turn design concepts into reality. Cadence customers are the world's most innovative companies, delivering extraordinary electronic products from chips to boards to systems for the most dynamic market applications including consumer, hyperscale computing, 5G communications, automotive, aerospace, industrial and healthcare.",
-    name: "CADENCE DESIGN SYSTEMS",
-    zone: "SUNLIGHT ZONE",
+    text: "EY provides consulting, assurance, tax and transaction services that help solve challenges and build a better working world for all. The insights and services EY provides helps to create long-term value for society and to build trust in the capital markets.",
+    name: "EY",
+    zone: "Platinum",
     image: "/logos/cadence.png",
     learnMore: "31 hp",
 }, {
-    text: "Ford Motor Company is an automobile company that designs, manufactures, markets, and services a full line of Ford trucks, utility vehicles, cars as well as Lincoln luxury vehicles.",
-    name: "FORD MOTOR COMPANY",
-    zone: "SUNLIGHT ZONE",
-    image: "/logos/Ford.png",
-    learnMore: "31 hp",
-},{
-    text: "With more than 35 years of precision machining experience, PALM Industries LLC. offers in-house design support, prototype, production and assembly of custom parts and systems. At PALM Industries, LLC. They specialize in highly processed parts requiring multiple machining and processing operations which involve exotic to conventional materials, and numerous outside services.",
-    name: "PALM INDUSTRIES",
-    zone: "SUNLIGHT ZONE",
-    image: "/logos/Palm.png",
-    learnMore: "31 hp",
-},{
-    text: "Siemens Teamcenter, NX, and Simcenter provide industry-leading project management, design, and simulation capabilities.",
-    name: "SIEMENS",
-    zone: "SUNLIGHT ZONE",
-    image: "/logos/Siemens.png",
-    learnMore: "31 hp",
-},{
-    text: "Brunswick is defining, creating, and innovating the future of recreational boating. With advanced design, technology and innovation, they will redefine recreational boating and advance their leadership position in the marine industry.",
-    name: "BRUNSWICK",
-    zone: "SUNLIGHT ZONE",
-    image: "/logos/Brunswick.png",
-    learnMore: "31 hp",
-},{
-    text: "Ilmor is a manufacturer of high-performance racing and recreational marine engines, with a drive for pushing the boundaries of innovative engineering in everything they do.",
-    name: "ILMOR ENGINEERING",
-    zone: "SUNLIGHT ZONE",
-    image: "/logos/Ilmor.png",
-    learnMore: "31 hp",
-},{
-    text: "A progressive, family-owned company located in Decatur, Indiana, Thunderbird Products produces the world-renowned lines of Formula Boats. With a long-established philosophy of uncompromising quality in every detail, Formula is recognized as the world's premier powerboat manufacturer.",
-    name: "FORMULA BOATS",
-    zone: "SUNLIGHT ZONE",
-    image: "/logos/Formula.png",
-    learnMore: "31 hp",
-},{
-    text: "Pure Watercraft is enabling a new era in boating that is more enjoyable, accessible, and environmentally-friendly than ever before. Founded in 2011 in Seattle, Washington, the company delivers an innovative electric marine propulsion system that preserves the air and waterways for future generations. In addition, Pure Watercraft and General Motors are collaborating on a shared vision to promote sustainability through an expansion of zero-emissions mobility and reflects the holistic approach necessary for widespread EV adoption.",
-    name: "PURE WATERCRAFT",
-    zone: "SUNLIGHT ZONE",
-    image: "/logos/PureWatercraft.png",
-    learnMore: "31 hp",
-},{
-    text: "RedViking is a provider of manufacturing and test solutions for the automotive, aerospace, heavy equipment, and defense industries. RedViking is Your Proven Partner for Manufacturing Solutions, Dynamic Test Systems, Operational Intelligence, and Assembly Solutions.",
-    name: "RED VIKING",
-    zone: "SUNLIGHT ZONE",
-    image: "/logos/RedViking.png",
+    text: "Cadence is a pivotal leader in electronic systems design, building upon more than 30 years of computational software expertise. Cadence applies its underlying Intelligent System Design strategy to deliver software, hardware, and IP that turn design concepts into reality.",
+    name: "Cadence",
+    zone: "Platinum",
+    image: "/logos/cadence.png",
     learnMore: "31 hp",
 }]
 
 const twilightSponsors = [{
-    text: "Bristol Harbor Group, Inc. (BHGI) is a full-service naval architecture, marine engineering and consulting firm located on the harbor in Bristol, Rhode Island. They have been in business for over twenty-five years and have produced numerous designs, to which hundreds of vessels have been built. They specialize in commercial vessel design and consulting and have experience with tugs, barges, Articulated Tug/Barge Units (ATB), passenger vessels, workboats, dredges, and floating dry docks.",
-    name: "BRISTOL HARBOR GROUP",
-    zone: "TWILIGHT ZONE",
+    text: "Axial flux e-motors are the next step in electric propulsion solutions. Used in marine, aviation, automotive and racing industries across the globe. Beyond Motors have the best continuous power-to-weight ratio on the market, and work in the most robust applications.",
+    name: "Beyond Motors",
+    zone: "Gold",
     image: "/logos/BristolHarborGroup.png",
-    learnMore: "",
-}, {
-    text: "ASNE is the leading professional engineering society for engineers, scientists and allied professionals who conceive, design, develop, test, construct, outfit, operate and maintain complex naval and maritime ships, submarines and aircraft and their associated systems and subsystems.",
-    name: "AMERICAN SOCIETY OF NAVAL ENGINEERS (ASNE)",
-    zone: "TWILIGHT ZONE",
-    image: "/logos/AmericanSocietyOfNavalEng.png",
-    learnMore: "",
-}, {
-    text: "DHX Machines designs, builds, and tests state-of-the-art electric machines incorporating the patented, proprietary, micro-feature enhanced DwHX cooling technology.",
-    name: "DHX MACHINES",
-    zone: "TWILIGHT ZONE",
-    image: "/logos/DHXMachines.png",
-    learnMore: "",
-}, {
-    text: "Vector supports manufacturers and suppliers of the automotive industry and related industries with a professional platform of tools, software components and services for developing embedded systems.\",",
-    name: "VECTOR",
-    zone: "TWILIGHT ZONE",
-    image: "/logos/Vector.png",
-    learnMore: "",
-}, {
-    text: "Vector supports manufacturers and suppliers of the automotive industry and related industries with a professional platform of tools, software components and services for developing embedded systems.",
-    name: "MICHIGAN WHEEL",
-    zone: "TWILIGHT ZONE",
-    image: "/logos/MichiganWheel.png",
-    learnMore: "",
-}, {
-    text: "System & software architecture engineering consulting firm.",
-    name: "QUANTUM WHATEVER, LLC",
-    zone: "TWILIGHT ZONE",
-    image: "/logos/QuantumWhatever.png",
     learnMore: "",
 }]
 
@@ -116,18 +44,6 @@ const midnightZone = [{
     url: "/sponsors/UmichECSG.png",
 },{
     url: "/sponsors/UmichCE.png",
-},{
-    url: "/sponsors/GM.png",
-},{
-    url: "/sponsors/Tekton.png",
-},{
-    url: "/sponsors/UltraMotion.png",
-},{
-    url: "/sponsors/Phase3.png",
-},{
-    url: "/sponsors/KleanKanteen.png",
-},{
-    url: "/sponsors/BlackVeatch.png",
 }]
 
 const hadalZone = [{
@@ -136,14 +52,6 @@ const hadalZone = [{
     url: "/sponsors/UmichCSE.png",
 },{
     url: "/sponsors/Kvaser.png",
-},{
-    url: "/sponsors/RedBull.png",
-},{
-    url: "/sponsors/Celcius.png",
-},{
-    url: "/sponsors/BoeShield.png",
-},{
-    url: "/sponsors/Axiomatic.png",
 }]
 
 
@@ -177,7 +85,7 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="text-center p-10">
-                    <h1 className="lg:text-6xl text-4xl  font-bold">Sunlight Zone Sponsors</h1>
+                    <h1 className="lg:text-6xl text-4xl  font-bold"> Platinum Sponsors</h1>
                     <div>
                         {sunlightSponsors.map((sponsor: { text: any; name: any; zone: any; image: any; learnMore: any; }, index: any) => (
                             <SponsorCard
@@ -193,7 +101,7 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="text-center p-10">
-                    <h1 className="lg:text-6xl text-4xl  font-bold">Twilight Zone Sponsors</h1>
+                    <h1 className="lg:text-6xl text-4xl  font-bold">Gold Sponsors</h1>
                     <div>
                         {twilightSponsors.map((sponsor: { text: any; name: any; zone: any; image: any; learnMore: any; }, index: any) => (
                             <SponsorCard
@@ -209,7 +117,7 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="text-center p-10">
-                    <h1 className="lg:text-6xl text-4xl pb-20 font-bold"> Midnight Zone Sponsors </h1>
+                    <h1 className="lg:text-6xl text-4xl pb-20 font-bold"> Silver Sponsors </h1>
                     <div className={"grid md:grid-cols-3 gap-5 lg:px-20 md:px-10"}>
                         {midnightZone.map((sponsor: { url: any}) => (
                             <div key={0} className={"flex justify-center"}>
@@ -220,7 +128,7 @@ export default function Home() {
                 </div>
 
                 <div className="text-center p-10">
-                    <h1 className="lg:text-6xl text-4xl pb-20 font-bold"> Hadal Zone Sponsors </h1>
+                    <h1 className="lg:text-6xl text-4xl pb-20 font-bold"> Bronze Sponsors </h1>
                     <div className={"grid md:grid-cols-3 gap-5 lg:px-20 md:px-10"}>
                         {hadalZone.map((sponsor: { url: any}) => (
                             <div key={0} className={"flex justify-center"}>
