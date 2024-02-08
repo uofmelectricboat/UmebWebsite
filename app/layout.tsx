bc511bdf85785e5f6a4709218780c21a73ced5ef
apalo <apalo@umich.edu>
2024-02-08 09:38:56 -0500

    font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ import { Nunito } from 'next/font/google'
 import Navbar from './Components/Navbar/navbar'
 import Header from './Components/Header'
 import Footer from './Components/Footer/footer'
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  weight: '400',
+  display: 'swap',
+  subsets: ['latin'],
+})
 
 export const metadata = {
   title: 'UMEB',
@@ -15,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={montserrat.className}>
       <body>
         <Header></Header>
         {children}
