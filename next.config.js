2ddeca5bb3bece2d9a1dbab33cbe58aeafb17cd0
Alec <alecpalo@Alecs-MacBook-Pro-4.local>
2024-05-23 12:11:51 -0400

    updated json

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/test",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+    // basePath: "/test",
+    // output: "export",  // <=== enables static exports
+    // reactStrictMode: true,
 };
 
 module.exports = nextConfig
